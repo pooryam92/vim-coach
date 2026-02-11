33fca4359bac01269639801c1599a27043c9b221
PooryaCareExpert <p.mohammadi@care-expert.nl>
2026-02-11 09:38:39 +0100

    beta

--- src/main/kotlin/com/github/pooryam92/vimcoach/config/VimTipConfig.kt
+++ src/main/kotlin/com/github/pooryam92/vimcoach/config/VimTipConfig.kt
@@ -2,7 +2,7 @@ package com.github.pooryam92.vimcoach.config
 
 object VimTipConfig {
     const val REMOTE_URL =
-        "https://raw.githubusercontent.com/pooryam92/vim-coach/wip/tips/vim_tips.json"
+        "https://raw.githubusercontent.com/pooryam92/vim-coach/main/tips/vim_tips.json"
     const val CONNECT_TIMEOUT_MS = 1000
     const val READ_TIMEOUT_MS = 1500
 }
