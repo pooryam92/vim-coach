d32407c1dc555bd7f59e835b8e832726c1673859
Poorya Mohammadi <poorya922@gmail.com>
2026-02-18 10:10:01 +0100

    update github url for tips

--- src/main/kotlin/com/github/pooryam92/vimcoach/config/VimTipConfig.kt
+++ src/main/kotlin/com/github/pooryam92/vimcoach/config/VimTipConfig.kt
@@ -2,5 +2,5 @@ package com.github.pooryam92.vimcoach.config
 
 object VimTipConfig {
     const val TIPS_FILE_PATH_PROPERTY = "vimcoach.tip.file.path"
-    const val GITHUB_API_URL = "https://api.github.com/repos/pooryam92/vim-coach/contents/tips/vim_tips.json"
+    const val GITHUB_API_URL = "https://api.github.com/repos/pooryam92/vim-coach/contents/tips/vim_tips_min.json"
 }
